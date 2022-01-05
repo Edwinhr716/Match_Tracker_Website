3f9a61853ee024d8b11f0b839ddce41f0b0f3785
Edwin Hernandez <86417275+Edwinhr716@users.noreply.github.com>
2022-01-05 12:32:17 -0600

    Update SQL_Connection.py

--- data_retrieval/SQL_Connection.py
+++ data_retrieval/SQL_Connection.py
@@ -4,7 +4,7 @@ import mysql.connector
 class mySQLServer:
 
     def __init__(self):
-        self.cnx = mysql.connector.connect(user='root', password='Rocco99mascota!',
+        self.cnx = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
                               database='matches')
 
